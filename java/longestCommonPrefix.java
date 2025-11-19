b37e811b0660afd7b83689e20aec22c9bc5e76a1
arya0509 <151792793+arya0509@users.noreply.github.com>
2025-11-19 10:50:03 -0700

    Completed longestCommonPrefix.java

--- java/longestCommonPrefix.java
+++ java/longestCommonPrefix.java
@@ -1,5 +1,25 @@
-public class longestCommonPrefix {
-    public static void solution() {
+import java.util.Arrays;
 
+public class longestCommonPrefix {
+    public static void main(String[] args) {
+        System.out.println(solution(new String[] { "abab", "aba", "" }));
+    }
+
+    public static String solution(String[] strs) {
+        String commonPrefix = "";
+        Arrays.sort(strs);
+
+        char[] arr1 = strs[0].toCharArray();
+        char[] arr2 = strs[strs.length - 1].toCharArray();
+
+        int shortestLength = arr1.length < arr2.length ? arr1.length : arr2.length;
+
+        for (int i = 0; i < shortestLength; i++) {
+            if (arr1[i] != arr2[i]) {
+                break;
+            }
+            commonPrefix += arr1[i];
+        }
+        return commonPrefix;
     }
 }
